12ca6baab16d9276af1e2e0c23f4e1d64bf4baba
v.loitp <v.Loitp@cloudhms.net>
2020-10-09 15:37:54 +0700

    update

--- app/src/main/java/com/loitp/activity/MainActivity.kt
+++ app/src/main/java/com/loitp/activity/MainActivity.kt
@@ -14,6 +14,7 @@ import com.core.base.BaseFontActivity
 import com.core.common.Constants
 import com.core.helper.adhelper.AdHelperActivity
 import com.core.helper.gallery.GalleryCoreSplashActivity
+import com.core.helper.girl.ui.GirlSplashActivity
 import com.core.utilities.*
 import com.google.android.material.navigation.NavigationView
 import com.loitp.R
@@ -112,7 +113,20 @@ class MainActivity : BaseFontActivity(), NavigationView.OnNavigationItemSelected
                 }
             }
             R.id.navGallery18 -> {
-                //TODO
+                if (Constants.IS_DEBUG) {
+                    val intent = Intent(this, GirlSplashActivity::class.java)
+                    intent.putExtra(Constants.AD_UNIT_ID_BANNER, getString(R.string.str_b))
+
+                    val listBkg = ArrayList<String>()
+                    listBkg.add("https://live.staticflickr.com/4657/26146170428_894243ab3c_b.jpg")
+                    listBkg.add("https://live.staticflickr.com/4782/26128440717_a00e7cbec1_h.jpg")
+                    listBkg.add("https://live.staticflickr.com/817/26128440867_1a90f7f8ec_h.jpg")
+                    listBkg.add("https://live.staticflickr.com/789/26128436937_84ecab7cdf_h.jpg")
+                    listBkg.add("https://live.staticflickr.com/4794/26128436737_69e5dfca7b_h.jpg")
+                    intent.putExtra(Constants.BKG_SPLASH_SCREEN, listBkg.random())
+                    startActivity(intent)
+                    LActivityUtil.tranIn(this)
+                }
             }
             R.id.navRateApp -> {
                 LSocialUtil.rateApp(activity = this, packageName = packageName)
