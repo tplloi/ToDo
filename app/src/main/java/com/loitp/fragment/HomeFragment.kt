61448d0832d9888889660777d9e56466e4cafb7f
Loitp <v.loitp@cloudhms.net>
2021-12-05 20:39:49 +0700

    update

--- app/src/main/java/com/loitp/fragment/HomeFragment.kt
+++ app/src/main/java/com/loitp/fragment/HomeFragment.kt
@@ -6,6 +6,7 @@ import com.annotation.LogTag
 import com.core.base.BaseFragment
 import com.loitp.R
 import com.loitp.viewmodels.MainViewModel
+import kotlinx.android.synthetic.main.frm_home.*
 
 @LogTag("HomeFragment")
 class HomeFragment : BaseFragment() {
@@ -24,6 +25,19 @@ class HomeFragment : BaseFragment() {
     }
 
     private fun setupViews() {
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navAll -> {
+                }
+                R.id.navComplete -> {
+                }
+                R.id.navIncomplete -> {
+                }
+                else -> {
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 
     private fun setupViewModels() {
