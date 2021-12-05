7e4a8b9b2436ae5c0fcd25480d5b035d123e9010
Loitp <v.loitp@cloudhms.net>
2021-12-05 20:13:27 +0700

    update

--- app/src/main/java/com/loitp/fragment/HomeFragment.kt
+++ app/src/main/java/com/loitp/fragment/HomeFragment.kt
@@ -2,15 +2,10 @@ package com.loitp.fragment
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
 import com.annotation.LogTag
-import com.core.base.BaseApplication
 import com.core.base.BaseFragment
-import com.core.utilities.LSocialUtil
 import com.loitp.R
 import com.loitp.viewmodels.MainViewModel
-import com.views.setSafeOnClickListener
-import kotlinx.android.synthetic.main.frm_home.*
 
 @LogTag("HomeFragment")
 class HomeFragment : BaseFragment() {
@@ -22,9 +17,6 @@ class HomeFragment : BaseFragment() {
 
         setupViews()
         setupViewModels()
-        context?.let {
-            mainViewModel?.loadListChap()
-        }
     }
 
     override fun setLayoutResourceId(): Int {
@@ -32,31 +24,11 @@ class HomeFragment : BaseFragment() {
     }
 
     private fun setupViews() {
-        btGithub.setSafeOnClickListener {
-            LSocialUtil.openUrlInBrowser(
-                context = activity,
-                url = "https://github.com/tplloi/basemaster.demo"
-            )
-        }
     }
 
     private fun setupViewModels() {
         mainViewModel = getViewModel(MainViewModel::class.java)
         mainViewModel?.let { mvm ->
-            mvm.eventLoading.observe(
-                viewLifecycleOwner,
-                { isLoading ->
-                    indicatorView.isVisible = isLoading
-                }
-            )
-
-            mvm.listChapLiveData.observe(
-                viewLifecycleOwner,
-                { listChap ->
-                    logD("<<<listChapLiveData " + BaseApplication.gson.toJson(listChap))
-                    btGithub.visibility = View.VISIBLE
-                }
-            )
         }
     }
 }
