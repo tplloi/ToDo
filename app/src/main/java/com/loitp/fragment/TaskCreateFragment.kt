7b5442d5c735aae552ecc8ddc0d0436f0075c289
Loitp <v.loitp@cloudhms.net>
2021-12-05 21:30:25 +0700

    update

--- app/src/main/java/com/loitp/fragment/TaskCreateFragment.kt
+++ app/src/main/java/com/loitp/fragment/TaskCreateFragment.kt
@@ -1,11 +1,14 @@
 package com.loitp.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.loitp.R
+import kotlinx.android.synthetic.main.frm_task_create.*
 
 class TaskCreateFragment : BottomSheetDialogFragment() {
 
@@ -14,7 +17,22 @@ class TaskCreateFragment : BottomSheetDialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.frm_task_create, container, false)
     }
-} // Required empty public constructor
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupViews() {
+        etContent.setOnTouchListener { view, event ->
+            view.parent.requestDisallowInterceptTouchEvent(true)
+            if ((event.action and MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
+                view.parent.requestDisallowInterceptTouchEvent(false)
+            }
+            return@setOnTouchListener false
+        }
+    }
+}
