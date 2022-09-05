236a6741690292d08ee176e8aff4f1deb7146cf8
loitp <loitp@galaxy.one>
2022-09-05 11:07:52 +0700

    update

--- app/src/main/java/com/roy93group/app/LApplication.kt
+++ app/src/main/java/com/roy93group/app/LApplication.kt
@@ -1,18 +1,18 @@
 package com.roy93group.app
 
-import com.roy93group.db.TaskDatabase
 import com.loitpcore.annotation.LogTag
 import com.loitpcore.core.base.BaseApplication
 import com.loitpcore.core.common.Constants
 import com.loitpcore.core.utilities.LUIUtil
 import com.loitpcore.data.ActivityData
+import com.roy93group.db.TaskDatabase
 
-//TODO ic_launcher
 //TODO new keystore
 //TODO refactor codes
 
 //DONE
 //change pkg name
+//TODO ic_launcher
 
 @LogTag("LApplication")
 class LApplication : BaseApplication() {
